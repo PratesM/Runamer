5e1109a4625d2f321f2e0cdccbab047d9137cdad
Prates <mgp.prates@gmail.com>
2021-11-04 18:12:29 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+// Bulk file namer
+// TODO: process args first
+
+use core::panic;
+use std::fs;
+use std::fs::read_dir;
+use std::fs::File;
+use std::{env, path};
+use std::path::{Path, PathBuf};
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+
+}
